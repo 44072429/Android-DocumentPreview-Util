6407ff146d356c5c44789dc0a67c21fd8b6932b0
18758 <44072429@qq.com>
2018-06-15 15:28:44 +0800

    添加 Android 根据JitPack 生成 gradule 方式发布库 的说明文档

--- document-util/src/main/java/util/android/ys/com/document_util/LoadFileModel.java
+++ document-util/src/main/java/util/android/ys/com/document_util/LoadFileModel.java
@@ -6,7 +6,7 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+//import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by 12457 on 2017/8/21.
@@ -17,15 +17,15 @@ public class LoadFileModel {
 
     public static void loadPdfFile(String url, Callback<ResponseBody> callback) {
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://www.baidu.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        LoadFileApi mLoadFileApi = retrofit.create(LoadFileApi.class);
-        if (!TextUtils.isEmpty(url)) {
-            Call<ResponseBody> call = mLoadFileApi.loadPdfFile(url);
-            call.enqueue(callback);
-        }
+//        Retrofit retrofit = new Retrofit.Builder()
+//                .baseUrl("https://www.baidu.com/")
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .build();
+//        LoadFileApi mLoadFileApi = retrofit.create(LoadFileApi.class);
+//        if (!TextUtils.isEmpty(url)) {
+//            Call<ResponseBody> call = mLoadFileApi.loadPdfFile(url);
+//            call.enqueue(callback);
+//        }
 
     }
 }
