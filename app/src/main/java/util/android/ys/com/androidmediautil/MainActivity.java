9652538f67ddca0426abdf9d5aab88181566e576
18758 <44072429@qq.com>
2018-06-15 13:15:23 +0800

    添加 Android 根据JitPack 生成 gradule 方式发布库 的说明文档

---
+++ app/src/main/java/util/android/ys/com/androidmediautil/MainActivity.java
@@ -0,0 +1,13 @@
+package util.android.ys.com.androidmediautil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
